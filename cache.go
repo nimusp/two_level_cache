eb21938487a8e685d7393543ec15a34b22fc32c2
Pavel <harrison754@icloud.com>
2019-08-11 17:26:54 +0300

    Добавлена выгрузка, добавлена запись в файлы

--- cache.go
+++ cache.go
@@ -1,10 +1,16 @@
 package cache
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"sync"
 	"time"
 )
+
+const dataPath = "data"
 
 type сacheItem struct {
 	Value    interface{}
@@ -38,51 +44,126 @@ func InitNewCache(checkInterval time.Duration, inMemoryTime time.Duration) *Cach
 }
 
 func (c *Cache) Add(key string, item interface{}) {
-	cacheItem := сacheItem{
-		Value:    item,
-		PuttedAt: time.Duration(time.Now().Nanosecond()),
-	}
 	c.Mutex.Lock()
 	defer c.Mutex.Unlock()
-	c.Data[key] = cacheItem
+	c.addToMap(key, item)
 }
 
 func (c *Cache) Get(key string) (interface{}, error) {
 	c.Mutex.Lock()
 	defer c.Mutex.Unlock()
 
-	value, isExist := c.Data[key]
+	valueItem, isExist := c.Data[key]
+	var value interface{}
 	if !isExist {
-		return nil, errors.New("Key not found")
+		value, isExist = c.readFromFile(key)
+		if !isExist {
+			return nil, errors.New("Key not found")
+		}
+		c.addToMap(key, value)
+	} else {
+		value = valueItem.Value
 	}
-	return value.Value, nil
+
+	return value, nil
 }
 
-func (c *Cache) Delete(key string) error {
+func (c *Cache) Delete(key string) {
 	c.Mutex.Lock()
 	defer c.Mutex.Unlock()
 
-	_, isExist := c.Data[key]
-	if !isExist {
-		return errors.New("Key not found")
-	}
-
+	c.removeFromFile(key)
 	delete(c.Data, key)
-	return nil
+}
+
+func (c *Cache) GetOnlyInMemory() map[string]сacheItem {
+	return c.Data
 }
 
 func (c *Cache) startItemCleaner() {
-	ticker := time.NewTicker(c.CheckInterval)
 	go func() {
+		ticker := time.NewTicker(c.CheckInterval)
 		for range ticker.C {
-			now := time.Duration(time.Now().Nanosecond())
+			now := time.Now().Unix()
+			mapToSave := make(map[string]interface{})
 			for k, v := range c.Data {
-				if v.PuttedAt+c.InMemoryTime < now {
+				at := int64(v.PuttedAt)
+				inMemoryTime := int64(c.InMemoryTime / time.Second)
+				if at+inMemoryTime < now {
 					c.Mutex.Lock()
-					delete(c.Data, k)
+					mapToSave[k] = v.Value
 					c.Mutex.Unlock()
 				}
 			}
+			if len(mapToSave) > 0 {
+				c.Mutex.Lock()
+				fmt.Println("mapToSave len", len(mapToSave))
+				for k, v := range mapToSave {
+					c.writeToFile(k, v)
+					delete(c.Data, k)
+					delete(mapToSave, k)
+				}
+				c.Mutex.Unlock()
+			}
 		}
 	}()
+}
+
+func (c *Cache) addToMap(key string, item interface{}) {
+	cacheItem := сacheItem{
+		Value:    item,
+		PuttedAt: time.Duration(time.Now().Unix()),
+	}
+	c.Data[key] = cacheItem
+}
+
+func (c *Cache) readFromFile(key string) (interface{}, bool) {
+	fileDir := c.getFileDir() + key
+
+	data, err := ioutil.ReadFile(fileDir)
+	if err != nil {
+		fmt.Println(err)
+		return nil, false
+	}
+	var result interface{}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, false
+	}
+	fmt.Println("Read from file", result)
+	return result, true
+}
+
+func (c *Cache) writeToFile(key string, value interface{}) error {
+	rawData, err := json.Marshal(value)
+
+	_, err = os.Stat(dataPath)
+	isNotExist := os.IsNotExist(err)
+	if isNotExist {
+		os.Mkdir(dataPath, 0777)
+	}
+
+	fileDir := c.getFileDir() + key
+	_, err = os.Stat(fileDir)
+	isExist := !os.IsNotExist(err)
+	if isExist {
+		os.Remove(fileDir)
+	}
+	err = ioutil.WriteFile(fileDir, rawData, 0644)
+	fmt.Println("Wrote to file", value)
+	return err
+}
+
+func (c *Cache) removeFromFile(key string) {
+	fileDir := c.getFileDir() + key
+	_, err := os.Stat(fileDir)
+	if os.IsNotExist(err) {
+		return
+	}
+	os.Remove(fileDir)
+}
+
+func (c *Cache) getFileDir() string {
+	currentDir, _ := os.Getwd()
+	return currentDir + string(os.PathSeparator) + dataPath + string(os.PathSeparator)
 }
