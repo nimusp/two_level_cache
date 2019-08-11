bae475806c5ea485a33f8e7fd015e196ad7d7a04
Pavel <harrison754@icloud.com>
2019-08-11 17:27:07 +0300

    Обновлены тесты

--- cache_test.go
+++ cache_test.go
@@ -6,11 +6,16 @@ import (
 )
 
 var cache *Cache
+var fastCache *Cache
 
 func init() {
 	checkIntervalTime := 1 * time.Second
 	holdInMemoryTime := 1 * time.Second
 	cache = InitNewCache(checkIntervalTime, holdInMemoryTime)
+
+	shortCheckIntervalTime := 1 * time.Millisecond
+	shortHoldInMemoryTime := 1 * time.Millisecond
+	fastCache = InitNewCache(shortCheckIntervalTime, shortHoldInMemoryTime)
 }
 
 func TestWrongInit1(t *testing.T) {
@@ -30,29 +35,41 @@ func TestWrongInit2(t *testing.T) {
 }
 
 func TestGet(t *testing.T) {
-	cache.Add("key", "test_data")
-	data, _ := cache.Get("key")
+	key := "key"
+	cache.Add(key, "test_data")
+	data, _ := cache.Get(key)
 	if data == nil {
 		t.Errorf("Data not saved %s", data)
 	}
 }
 
+func TestSlowGet(t *testing.T) {
+	key := "key_slow"
+	fastCache.Add(key, "test_data")
+
+	time.Sleep(1 * time.Second)
+
+	data, _ := fastCache.Get(key)
+	if data == nil {
+		t.Error("Data not saved", data)
+	}
+}
+
 func TestRewriting(t *testing.T) {
-	cache.Add("key_to_rewrite", 40)
-	cache.Add("key_to_rewrite", 42)
-	data, _ := cache.Get("key_to_rewrite")
+	key := "key_to_rewrite"
+	cache.Add(key, 40)
+	cache.Add(key, 42)
+	data, _ := cache.Get(key)
 	if data != 42 {
 		t.Errorf("Data don't rewriting")
 	}
 }
 
 func TestDelete(t *testing.T) {
-	cache.Add("key_to_delete", 42)
-	err := cache.Delete("key_to_delete")
-	if err != nil {
-		t.Error("Error on delete ", err)
-	}
-	data, err := cache.Get("hey_to_delete")
+	key := "key_to_delete"
+	cache.Add(key, 42)
+	cache.Delete(key)
+	data, err := cache.Get(key)
 	if err == nil {
 		t.Error("Don't get error while getting deleted data")
 	}
@@ -61,11 +78,41 @@ func TestDelete(t *testing.T) {
 	}
 }
 
-func TestCleaner(t *testing.T) {
-	cache.Add("cleaner_key", 42)
+func TestSaveToDisk(t *testing.T) {
+	key := "value_for_save_to_disk"
+	cache.Add(key, 42)
 	time.Sleep(2 * time.Second)
-	_, err := cache.Get("cleaner key")
+	_, err := cache.Get(key)
+	if err != nil {
+		t.Error("Don't save to disk", err)
+	}
+}
+
+func TestCacheClear(t *testing.T) {
+	fastCache.Add("some", 42)
+	fastCache.Add("another", "value")
+
+	currentData := fastCache.GetOnlyInMemory()
+	if !(len(currentData) > 0) {
+		t.Error("Empty data in memory")
+	}
+
+	time.Sleep(2 * time.Second)
+	afterIntervalData := cache.GetOnlyInMemory()
+	if len(afterIntervalData) > 0 {
+		t.Error("Data don't clear", afterIntervalData)
+	}
+}
+
+func TestRemoveFile(t *testing.T) {
+	key := "key_to_remove"
+	fastCache.Add(key, 7)
+
+	time.Sleep(1 * time.Second)
+
+	fastCache.Delete(key)
+	_, err := fastCache.Get(key)
 	if err == nil {
-		t.Error("Cleaner don't delete data")
+		t.Error("File was not removed")
 	}
 }
